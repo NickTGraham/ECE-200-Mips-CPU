44783b48b3215e552b71bcbd20660ee261417794
Nicholas Graham <nickdud341@gmail.com>
2016-04-05 21:49:02 -0400

    Fixed RegFile Testbench issues

--- RegFile.v
+++ RegFile.v
@@ -1,7 +1,7 @@
 module regfile (read_address_one, read_address_two, write_address, write_enable, write_data, A, B, clk);
     input [4:0] read_address_one, read_address_two, write_address;
     input write_enable, clk;
-    input [16:0] write_data;
+    input [15:0] write_data;
 
     output [15:0] A, B;
     wire [15:0] A, B;
@@ -29,7 +29,7 @@ module regfile (read_address_one, read_address_two, write_address, write_enable,
     always @(negedge clk )
     begin
     if(write_enable == 1)
-        data[read_address_one] <= write_data;
+        data[write_address] <= write_data;
     end
 
 endmodule
@@ -38,19 +38,19 @@ module RegTestbench ();
     reg [4:0] addr_one, addr_two, write_addr;
     wire [15:0] A, B;
     reg clk, we;
-    reg [16:0] wd;
+    reg [15:0] wd;
     regfile t(addr_one, addr_two, write_addr, we, wd, A, B, clk);
     initial begin
         addr_one <= 4'b0000;
         addr_two <= 4'b0000;
-        write_addr <= 4'b0000;
+        write_addr <= 0;
         wd = 8;
-        clk <= 0;
+        clk <= 1;
         we <= 1;
     end
 
     always begin
-        #100 clk <= !clk;
+        #200 clk <= !clk;
     end
     always @(posedge clk)
     begin
@@ -58,13 +58,17 @@ module RegTestbench ();
             $monitor ($time, "s %b %b", A, B);
             addr_one = addr_one + 1;
             addr_two = addr_two + 1;
-            write_addr = write_addr + 1;
         end
         else begin
-            we = 0;
             addr_one = 4'b0000;
             addr_two = 4'b0000;
         end
 
+    end
+
+    always @ ( negedge clk ) begin
+        if(write_addr < 20) begin
+            write_addr = write_addr + 1;
+        end
     end
 endmodule
